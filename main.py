e1c0ac72e79d0d8935266b623c7120eec1025bb6
diegoseg_15 <diegofersv@gmail.com>
2024-05-03 22:50:29 -0500

    :wave: Create Item component

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from reactpy import component, html
+from reactpy import component, html, hooks
 from reactpy.backend.fastapi import configure
 from fastapi import FastAPI
 
 @component
+def Item(text, initial_done=False):
+    done, set_done = hooks.use_state(initial_done)
+    
+    def handle_click(event):
+        set_done(not done)
+    
+    attrs = {"style":{"color":"green"}} if done else {}
+    
+    if done:
+        return html.li(attrs, text)
+    else:
+        return html.li(
+            html.span(attrs, text),
+            html.button({"on_click": handle_click}, "Hecho!")
+        )
+
+@component
 def HelloWorld():
-    return html.div(
+    return html._(
         html.h1("Lista de Tareas"),
-        html.lu(
-            html.li("Tarea 1"),
-            html.li("Tarea 2"),
+        html.ul(
+            Item("Aprender React con Python 1"),
+            Item("Aprender React con Python 2"),
+            Item("Aprender más sobre Python", initial_done=True),
         )
     )
 
