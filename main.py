3767676ab39710c31c5a631e23e22341b92a664d
diegoseg_15 <diegofersv@gmail.com>
2024-05-03 23:43:55 -0500

    :lipstick: Style with Tailwind CSS

--- main.py
+++ main.py
@@ -15,25 +15,34 @@ def Item(text, initial_done=False):
     def handle_click(event):
         set_done(not done)
     
-    attrs = {"style":{"color":"green"}} if done else {}
+    attrs = {"class":"bg-green-600 text-white py-4 px-5 font-semibold border-b-2"} if done else {"class":"flex space-x-5 items-center py-4 px-5 border-b-2"}
     
     if done:
         return html.li(attrs, text)
     else:
-        return html.li(
-            html.span(attrs, text),
-            html.button({"on_click": handle_click}, "Hecho!")
+        return html.li(attrs,
+            html.span(text),
+            html.button({"class":"px-3 py-2 bg-green-600 rounded-lg font-semibold text-white","on_click": handle_click}, "Relizado"),
         )
 
 @component
 def HelloWorld():
     return html._(
         cdn_l,
-        html.h1("Lista de Tareas"),
-        html.ul(
-            Item("Aprender React con Python 1"),
-            Item("Aprender React con Python 2"),
-            Item("Aprender más sobre Python", initial_done=True),
+        html.div(
+            {"class":"w-screen text-center py-5 bg-sky-800 text-white"},
+            html.h1({"class":"text-3xl font-bold"}, "Lista de Tareas"),
+        ),
+        
+        html.div(
+            {"class":"w-creen py-10 px-16"},
+            html.ul(
+                {"class":"list-disc flex flex-col space-y-3"},            
+                Item("Aprender React con Python 1"),
+                Item("Aprender React con Python 2"),
+                Item("Aprender más sobre Python", initial_done=True),
+            )
+
         )
     )
 
